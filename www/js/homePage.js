3f4e4a83ee40fe256a0942c746b7c36023652377
amcnulty <amcnulty88@swbell.net>
2015-12-15 22:11:54 -0600

    Resize and align home screen buttons
    The buttons on the home screen will be large enough for a touch screen
    and are centered on the display.
    
    Also in this update, the orientation changed event is being handled in
    javascript to adjust the display for landscape or portrait.

--- www/js/homePage.js
+++ www/js/homePage.js
@@ -1,7 +1,35 @@
 window.onload = function() {
+	
+	function calculateWindowSize() {
+		windowWidth = $(window).width();
+		windowHeight = $(window).height();
+		app.style.width = String(windowWidth) + "px";
+		app.style.height = String(windowHeight) + "px";
+	}
+	
+	function setButtonPadding() {
+		horizontalPadding = String(windowWidth / 8) + "px";
+		verticalPadding = String(windowHeight / 20) + "px";
+		for (var i = 0; i < buttons.length; i++) {
+			buttons[i].style.padding = verticalPadding + " " + horizontalPadding + " " + verticalPadding + " " + horizontalPadding;
+		}
+	}
+	
+	function orientChange() {
+		calculateWindowSize();
+		setButtonPadding();
+	}
+	
+	//		VARIABLES
 	var windowWidth = $(window).width();
 	var windowHeight = $(window).height();
 	var app = document.getElementById("app");
-	app.style.width = String(windowWidth) + "px";
-	app.style.height = String(windowHeight) + "px";
+	var horizontalPadding = 0;
+	var verticalPadding = 0;
+	var buttons = document.getElementsByClassName("navButs");
+	
+	// Event Listener for orientation change
+	window.addEventListener("orientationchange", orientChange, false);
+	
+	orientChange();
 }
